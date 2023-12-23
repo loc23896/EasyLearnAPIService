aa667bedb1f919336291967742993b00cf3c2abe
loc23896 <loc23896@gmail.com>
2023-12-23 15:50:24 +0700

    update @Service mapper for Minh by Loc

--- src/main/java/com/easy/learn/mapper/TrainerSalaryPaidSummaryMapper.java
+++ src/main/java/com/easy/learn/mapper/TrainerSalaryPaidSummaryMapper.java
@@ -2,7 +2,9 @@ package com.easy.learn.mapper;
 
 import com.easy.learn.dto.TrainerSalaryPaidSummaryDTO;
 import com.easy.learn.entity.TrainerSalaryPaidSummary;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TrainerSalaryPaidSummaryMapper extends AbstractMapper<TrainerSalaryPaidSummary, TrainerSalaryPaidSummaryDTO>{
     public TrainerSalaryPaidSummaryMapper(){
         super(TrainerSalaryPaidSummary.class,TrainerSalaryPaidSummaryDTO.class);
